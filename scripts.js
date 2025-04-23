868a0d73592633b5858b827fc669991b2d7d401c
valmir <valmir473355@gmail.com>
2025-04-23 14:09:41 -0300

    ajuste no código

--- scripts.js
+++ scripts.js
@@ -3,54 +3,37 @@ const data = new Date();
 const diaAtual = data.getDate();
 
 
-const segunda_daniel = "FOLGA";
-const terca_valmir = "FOLGA";
-const quarta_elias = "FOLGA";
-const quinta_carlinhos = "FOLGA";
-const sexta_valmir = "FOLGA";
-const sabado_daniel = "FOLGA";
-
-
-
-if(diaAtual === 23){
-    elias.innerText = `Elias está de ${quarta_elias} !`
-    color_elias.style.backgroundColor = "yellow"
-    alert("Elias está de folga!");
+if(diaAtual === 21){
+    daniel.innerText = `Daniel está de folga !`
+    color_daniel.style.backgroundColor = "yellow" 
 }
-
 
 if(diaAtual === 22){
-    valmir.innerText = `Valmir está de ${terca_valmir} !`
+    valmir.innerText = `Valmir está de folga !`
     color_valmir.style.backgroundColor = "yellow"
-    alert("Valmir está de folga!");
 }
 
-
-if(diaAtual === 21){
-    daniel.innerText = `Daniel está de ${segunda_daniel} !`
-    color_daniel.style.backgroundColor = "yellow" 
-    alert("Daniel está de folga!");
+if(diaAtual === 23){
+    elias.innerText = "Elias está de folga !"
+    color_elias.style.backgroundColor = "yellow"
 }
 
 
 if(diaAtual === 24){
-    carlinhos.innerText = `Carlinhos está de ${quinta_carlinhos} !`
+    carlinhos.innerText = `Carlinhos está de folga !`
     color_carlinhos.style.backgroundColor = "yellow"
-    alert("Carlinhos está de folga!");
 }
 
 
 if(diaAtual === 25){
-    valmir.innerText = `Valmir está de ${sexta_valmir} !`
+    valmir.innerText = `Valmir está de folga !`
     color_valmir.style.backgroundColor = "yellow"
-    alert("Valmir está de folga!");
 }
 
 
 if(diaAtual === 26){
-    daniel.innerText = `Daniel está de${sabado_daniel} !`
+    daniel.innerText = `Daniel está de folga !`
     color_daniel.style.backgroundColor = "yellow"
-    alert("Daniel está de folga!");
 }
 
 
