f7ae5f72f195baaead8960c9f19253db19c63e71
valmir <valmir473355@gmail.com>
2025-04-23 05:32:29 -0300

    two commit

--- scripts.js
+++ scripts.js
@@ -3,43 +3,48 @@ const data = new Date();
 const diaAtual = data.getDate();
 
 
-const segunda_elias = "FOLGA";
+const segunda_daniel = "FOLGA";
 const terca_valmir = "FOLGA";
-const quarta_daniel = "FOLGA";
+const quarta_elias = "FOLGA";
 const quinta_carlinhos = "FOLGA";
 const sexta_valmir = "FOLGA";
 const sabado_daniel = "FOLGA";
 
 
 
-if(diaAtual === 21){
-    elias.innerText = `Hoje é folga de ${segunda_elias} !`
+if(diaAtual === 23){
+    elias.innerText = `Elias está de ${quarta_elias} !`
+    color_elias.style.backgroundColor = "yellow" 
 }
 
 
 if(diaAtual === 22){
     valmir.innerText = `Valmir está de ${terca_valmir} !`
+    color_valmir.style.backgroundColor = "yellow"
 }
 
 
-if(diaAtual === 23){
-    daniel.innerText = `Daniel está de ${quarta_daniel} !`
-    color.style.backgroundColor = "yellow" 
+if(diaAtual === 21){
+    daniel.innerText = `Daniel está de ${segunda_daniel} !`
+    color_daniel.style.backgroundColor = "yellow" 
 }
 
 
 if(diaAtual === 24){
     carlinhos.innerText = `Carlinhos está de ${quinta_carlinhos} !`
+    color_carlinhos.style.backgroundColor = "yellow"
 }
 
 
 if(diaAtual === 25){
     valmir.innerText = `Valmir está de ${sexta_valmir} !`
+    color_valmir.style.backgroundColor = "yellow"
 }
 
 
 if(diaAtual === 26){
     daniel.innerText = `Daniel está de${sabado_daniel} !`
+    color_daniel.style.backgroundColor = "yellow"
 }
 
 
