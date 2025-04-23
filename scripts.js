4c011b8fe9070639396842465fd068af17a89c84
valmir <valmir473355@gmail.com>
2025-04-23 04:49:35 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,53 @@
+const data = new Date();
+
+const diaAtual = data.getDate();
+
+
+const segunda_elias = "FOLGA";
+const terca_valmir = "FOLGA";
+const quarta_daniel = "FOLGA";
+const quinta_carlinhos = "FOLGA";
+const sexta_valmir = "FOLGA";
+const sabado_daniel = "FOLGA";
+
+
+
+if(diaAtual === 21){
+    elias.innerText = `Hoje é folga de ${segunda_elias} !`
+}
+
+
+if(diaAtual === 22){
+    valmir.innerText = `Valmir está de ${terca_valmir} !`
+}
+
+
+if(diaAtual === 23){
+    daniel.innerText = `Daniel está de ${quarta_daniel} !`
+    color.style.backgroundColor = "yellow" 
+}
+
+
+if(diaAtual === 24){
+    carlinhos.innerText = `Carlinhos está de ${quinta_carlinhos} !`
+}
+
+
+if(diaAtual === 25){
+    valmir.innerText = `Valmir está de ${sexta_valmir} !`
+}
+
+
+if(diaAtual === 26){
+    daniel.innerText = `Daniel está de${sabado_daniel} !`
+}
+
+
+
+
+
+
+
+
+
+
