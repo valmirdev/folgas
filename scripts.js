fa21765d66a549b4fd36241fbfe4f9e50f00fc12
valmir <valmir473355@gmail.com>
2025-04-28 01:36:24 -0300

    centralizei a tabela

--- scripts.js
+++ scripts.js
@@ -3,41 +3,32 @@ const data = new Date();
 const diaAtual = data.getDate();
 
 
-if(diaAtual == 21){
+if(diaAtual == 30){
     daniel.innerText = `Daniel está de folga !`
     color_daniel.style.backgroundColor = "yellow" 
-}
-
-if(diaAtual == 22){
+}if(diaAtual == 3){
     valmir.innerText = `Valmir está de folga !`
     color_valmir.style.backgroundColor = "yellow"
-}
-
-if(diaAtual == 23){
+}if(diaAtual == 29 && 2){
     elias.innerText = "Elias está de folga !"
     color_elias.style.backgroundColor = "yellow"
-}
-
-
-if(diaAtual == 24){
+}if(diaAtual == 28 && 1){
     carlinhos.innerText = `Carlinhos está de folga !`
     color_carlinhos.style.backgroundColor = "yellow"
-}
-
-
-if(diaAtual == 25){
-    valmir.innerText = `Valmir está de folga !`
-    color_valmir.style.backgroundColor = "yellow"
-}
-
-
-if(diaAtual == 26){
-    daniel.innerText = `Daniel está de folga !`
-    color_daniel.style.backgroundColor = "yellow"
 }else{
     combinar.innerText = `A combinar...`
     combinar.style.color = "red"
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
