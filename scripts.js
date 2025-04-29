0ed6fd81481731eab2f94e8ffea54baba0a5369f
valmir <valmir473355@gmail.com>
2025-04-29 19:14:27 -0300

    layolt

--- scripts.js
+++ scripts.js
@@ -3,22 +3,24 @@ const data = new Date();
 const diaAtual = data.getDate();
 
 
-if(diaAtual == 30){
-    daniel.innerText = `Daniel está de folga !`
-    color_daniel.style.backgroundColor = "yellow" 
-}if(diaAtual == 3){
-    valmir.innerText = `Valmir está de folga !`
-    color_valmir.style.backgroundColor = "yellow"
-}if(diaAtual == 29 && 2){
-    elias.innerText = "Elias está de folga !"
-    color_elias.style.backgroundColor = "yellow"
-}if(diaAtual == 28 && 1){
-    carlinhos.innerText = `Carlinhos está de folga !`
-    color_carlinhos.style.backgroundColor = "yellow"
-}else{
-    combinar.innerText = `A combinar...`
-    combinar.style.color = "red"
+if (diaAtual == 30){//DANIEL//
+    daniel.innerText = `Daniel está de folga !`;
+    color_daniel.style.backgroundColor = "yellow";
+}else if (diaAtual == 29){//VALMIR//
+    valmir.innerText = `Valmir está de folga !`;
+    color_valmir.style.backgroundColor = "yellow";
+}else if (diaAtual == 28 && 3){//ELIAS//
+    elias.innerText = "Elias está de folga !";
+    color_elias.style.backgroundColor = "yellow";
+}else if (diaAtual == 1 && 2){//CARLINHOS//
+    carlinhos.innerText = `Carlinhos está de folga !`;
+    color_carlinhos.style.backgroundColor = "yellow";
+} else {//A COMBINAR//
+    combinar.innerText = `A combinar...`;
+    combinar.style.color = "red";
 }
+    
+
 
 
 
