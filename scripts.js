2c53c596286573b2445fb202d8efff3165c7df5c
valmir <valmir473355@gmail.com>
2025-05-03 18:35:37 -0300

    organizei

--- scripts.js
+++ scripts.js
@@ -3,28 +3,30 @@ const data = new Date();
 const diaAtual = data.getDate();
 
 
-if (diaAtual == 30){//DANIEL//
-    daniel.innerText = `Daniel está de folga !`;
-    color_daniel.style.backgroundColor = "yellow";
+
+
+if (diaAtual == 28){//ELIAS//
+    elias.innerText = "Elias está de folga !";
+    color_elias.style.backgroundColor = "yellow";
 }else if (diaAtual == 29){//VALMIR//
     valmir.innerText = `Valmir está de folga !`;
     color_valmir.style.backgroundColor = "yellow";
-}else if (diaAtual == 28 && 3){//ELIAS//
-    elias.innerText = "Elias está de folga !";
-    color_elias.style.backgroundColor = "yellow";
-}else if (diaAtual == 1 && 2){//CARLINHOS//
+}else if (diaAtual == 30){//DANIEL//
+    daniel.innerText = `Daniel está de folga !`;
+    color_daniel.style.backgroundColor = "yellow";
+}else if (diaAtual == 1){//CARLINHOS//
     carlinhos.innerText = `Carlinhos está de folga !`;
     color_carlinhos.style.backgroundColor = "yellow";
-} else {//A COMBINAR//
+}else if (diaAtual == 2){//CARLINHOS//
+    carlinhos.innerText = `Carlinhos está de folga !`;
+    color_carlinhos.style.backgroundColor = "yellow";
+}else if (diaAtual == 3){//ELIAS//
+    elias.innerText = "Elias está de folga !";
+    color_elias.style.backgroundColor = "yellow";
+}else {//A COMBINAR//
     combinar.innerText = `A combinar...`;
     combinar.style.color = "red";
 }
-    
-
-
-
-
-
 
 
 
